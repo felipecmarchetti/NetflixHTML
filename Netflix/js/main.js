ecef0d466e3ac19ab2406693036ed319e095c870
Felipe Cunha Marchetti <63081153+felipecmarchetti@users.noreply.github.com>
2021-02-25 21:01:07 -0300

    Add files via upload

---
+++ Netflix/js/main.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {    
+  var scroll = $(window).scrollTop();
+   //console.log(scroll);
+  if (scroll >= 50) {
+      //console.log('a');
+      $(".container").addClass("scrolled");
+  } else {
+      //console.log('a');
+      $(".container").removeClass("scrolled");
+  }
+});
